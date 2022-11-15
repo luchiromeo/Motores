a915d4b0ae08b1b9877d1d660e9686e861b3c421
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-11-15 16:47:40 -0300

    Agregue el script para la linterna y modifique al luz del juego

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    public Light luzLinterna;
+    public bool activLight;
+    public bool linternaEnMano;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && linternaEnMano == true)
+        {
+            activLight = !activLight;
+
+            if (activLight == true)
+            {
+                luzLinterna.enabled = true;
+            }
+
+            if (activLight == false)
+            {
+                luzLinterna.enabled = false;
+            }
+        }
+    }
+}
