37849996c3a4785c3b77c57b697a23800bb96693
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-11-16 22:04:24 -0300

    Agrego al script del jugador el daño

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class ControlJugador : MonoBehaviour
 {
     public float rapidezDesplazamiento = 10.0f;
-    void Start() 
-    { 
+    private int hp;
+    public bool Perdio = false;
+    void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
     }
     void Update()
-    { 
+    {
         float movimientoAdelanteAtras = Input.GetAxis("Vertical") * rapidezDesplazamiento;
         float movimientoCostados = Input.GetAxis("Horizontal") * rapidezDesplazamiento;
         movimientoAdelanteAtras *= Time.deltaTime;
         movimientoCostados *= Time.deltaTime;
         transform.Translate(movimientoCostados, 0, movimientoAdelanteAtras);
-        if (Input.GetKeyDown("escape")) 
-        { 
+        if (Input.GetKeyDown("escape"))
+        {
             Cursor.lockState = CursorLockMode.None;
-        } 
+        }
+    }
+    public void recibirDaño()
+    {
+
+        hp = hp - 10;
+
+        if (hp <= 0)
+        {
+            hp = 0;
+
+            Perdio = true;
+           
+        }
+    }
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            recibirDaño();
+        }
+
+
+
     }
 }
