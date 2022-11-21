a9c4ffffea5ec47caa4ba7527bbb2d751cf4068a
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-11-21 14:45:04 -0300

    Agregue un script de rotacion a los coleccionables

---
+++ Assets/Scripts/Rotador.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Rotador : MonoBehaviour
+{ 
+    void Update() 
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    } 
+}
