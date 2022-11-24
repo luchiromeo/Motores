f79e1a8e7608113ce434571ea0901f013ec0ed72
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-11-23 21:29:06 -0300

    Modifique el post-Processing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Start() 
-    { 
-        
+    private void Start()
+    {
+
     }
     void Update()
-    { 
-      
-        if (Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+    {
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
             GestorDeAudio.instancia.ReproducirSonido("Pasos");
 
-        } else if (Input.GetKeyUp(KeyCode.W)|| Input.GetKeyUp(KeyCode.S)|| Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        }
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             GestorDeAudio.instancia.PausarSonido("Pasos");
         }
     }
+    
 }
