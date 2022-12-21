d982bf92d51e4a3e42d29068a022fb8044708bae
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-12-21 19:05:03 -0300

    cree con probuilder un prefab de bateria y le agregue textura

--- Assets/Scripts/Linterna.cs
+++ Assets/Scripts/Linterna.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Linterna : MonoBehaviour
 {
     public Light luzLinterna;
     public bool activLight;
-    
+    public float cantBateria=100;
+    public float PerdidaBat = 0.5f;
+    public Text porcentaje;
 
     void Update()
     {
+        
+        cantBateria= Mathf.Clamp(cantBateria,0,100);
+        int ValorBat = (int)cantBateria;
+        porcentaje.text = ValorBat.ToString() + "%";
+        
         if (Input.GetKeyDown(KeyCode.F))
         {
             activLight = !activLight;
@@ -23,6 +31,27 @@ public class Linterna : MonoBehaviour
             {
                 luzLinterna.enabled = false;
             }
+        }
+
+        if (activLight == true && cantBateria > 0) //reduce la bateria
+        {
+            cantBateria -=PerdidaBat * Time.deltaTime;
+        }
+
+        if (cantBateria > 0 && cantBateria < 50)
+        {
+            luzLinterna.intensity = 15f;
+        }
+
+        if (cantBateria > 0 && cantBateria < 25)
+        {
+            luzLinterna.intensity = 10f;
+        }
+
+        if (cantBateria == 0)
+        {
+            luzLinterna.intensity = 0f;
+
         }
     }
 }
