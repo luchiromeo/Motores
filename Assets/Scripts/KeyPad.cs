5d1c5414ad1d776be4cd8ae4ebf3c0b8befe704e
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-12-22 14:38:10 -0300

    Botón de ejecutar terminado

--- Assets/Scripts/KeyPad.cs
+++ Assets/Scripts/KeyPad.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class KeyPad : MonoBehaviour
 {
     public TMPro.TMP_Text ANS;
+    private string Answer = "12345";
    public void number(int Number)
     {
         ANS.text += Number.ToString();
+    } 
+
+    public void Ejecutar()
+    {
+
+        if (ANS.text == Answer)
+        {
+            ANS.text = "OPEN";
+        }
+        else
+        {
+            ANS.text = "Incorrecto";
+        }
     }
 }
