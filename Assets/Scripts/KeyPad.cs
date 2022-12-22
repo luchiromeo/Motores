b0d4ca3eaea7f5c8b1ac23bd76172508183fd14d
luchiromeo <114606939+luchiromeo@users.noreply.github.com>
2022-12-22 15:22:06 -0300

    Cree una animacion para abrir la puerta e hice el codigo para que esta se abra al ingresar los numeros

--- Assets/Scripts/KeyPad.cs
+++ Assets/Scripts/KeyPad.cs
@@ -6,6 +6,7 @@ public class KeyPad : MonoBehaviour
 {
     public TMPro.TMP_Text ANS;
     private string Answer = "12345";
+    [SerializeField] private Animator Door;
    public void number(int Number)
     {
         ANS.text += Number.ToString();
@@ -17,10 +18,18 @@ public class KeyPad : MonoBehaviour
         if (ANS.text == Answer)
         {
             ANS.text = "OPEN";
+            Door.SetBool("open",true);
+            StartCoroutine("StopDoor");
         }
         else
         {
             ANS.text = "Incorrecto";
         }
+    }
+    IEnumerator StopDoor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Door.SetBool("open",false);
+        Door.enabled = false;
     }
 }
